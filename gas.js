d54f1d6a8cde7153e638a3f09c0ec6620188cc25
evelllyn <tinghsin027@gmail.com>
2022-12-22 13:56:08 +0800

    v1.0

--- gas.js
+++ gas.js
@@ -1,3 +1,11 @@
-/**取得試算表 */
-let ss = SpreadsheetApp.getActiveSpreadsheet();
-console.log(ss)
+function test(){
+  /**取得試算表 */
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  /**取得工作表 */
+  let ws = ss.getSheetByName('工作表4');
+  /**取得範圍 */
+  let range = ws.getRange(1,1);
+  /**寫資料 */
+  range.setValue('單位')
+
+  console.log(ss)
